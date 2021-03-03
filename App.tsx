8641c6e5d49532f190d31dc1d55aab280a4a70df
wilfft <68745408+wilfft@users.noreply.github.com>
2021-03-03 00:48:36 -0300

    adicionado fontes google fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Header from "./src/Header";
+import {
+  useFonts,
+  OpenSans_400Regular,
+  OpenSans_700Bold,
+} from "@expo-google-fonts/open-sans";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
       <Header />
--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,16 +1,28 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+
 const Header = () => {
   const styles = StyleSheet.create({
     container: {
       backgroundColor: "#Da5c5c",
+      paddingTop: 50,
+      flexDirection: "row",
+      justifyContent: "center",
       height: 96,
+    },
+    text: {
+      fontSize: 18,
+      fontWeight: "bold",
+      lineHeight: 25,
+      letterSpacing: -0.24,
+      color: "#fff",
+      marginLeft: 15,
     },
   });
   return (
     <View style={styles.container}>
-      <Image source={require("./../assets/logo.png")} />
-      <Text>Ws Delivery !</Text>
+      <Image source={require("../assets/logo.png")} />
+      <Text style={styles.text}>Ws Delivery !</Text>
     </View>
   );
 };
